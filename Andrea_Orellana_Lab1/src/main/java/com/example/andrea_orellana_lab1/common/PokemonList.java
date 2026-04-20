9dc1a8148f0d9751f5cf3bf3842cb869724204fe
Andrea-004 <00106123@uca.edu.sv>
2026-04-20 12:34:02 -0600

    feat - adding full pokemon list

--- Andrea_Orellana_Lab1/src/main/java/com/example/andrea_orellana_lab1/common/PokemonList.java
+++ Andrea_Orellana_Lab1/src/main/java/com/example/andrea_orellana_lab1/common/PokemonList.java
@@ -1,6 +1,7 @@
 package com.example.andrea_orellana_lab1.common;
 
 import com.example.andrea_orellana_lab1.domain.entities.Pokemon;
+import com.example.andrea_orellana_lab1.domain.entities.PokemonType;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -12,6 +13,42 @@ public class PokemonList {
 
     public PokemonList(){
         this.pokemons = new ArrayList<>();
+
+        this.pokemons.add(Pokemon.builder()
+                .name("Charizard")
+                .pokemonType(PokemonType.FIRE)
+                .pokemonDebilities(List.of(PokemonType.WATER, PokemonType.PLANT))
+                .zone("Forest")
+                .region("Kanto")
+                .build());
+
+        this.pokemons.add(Pokemon.builder()
+                .name("Drippy")
+                .pokemonType(PokemonType.GHOST)
+                .pokemonDebilities(List.of(PokemonType.GHOST, PokemonType.FAIRY))
+                .zone("Forest")
+                .region("Hoenn")
+                .build());
+
+        this.pokemons.add(Pokemon.builder()
+                .name("Squirtle")
+                .pokemonType(PokemonType.WATER)
+                .pokemonDebilities(List.of(PokemonType.FIRE, PokemonType.PLANT))
+                .zone("Sea")
+                .region("Johto")
+                .build());
+
+        this.pokemons.add(Pokemon.builder()
+                .name("Darkrai")
+                .pokemonType(PokemonType.DARK)
+                .pokemonDebilities(List.of(PokemonType.FIRE, PokemonType.FAIRY))
+                .zone("Cave")
+                .region("Kanto")
+                .build());
+    }
+
+    public List<Pokemon>getPokemons() {
+        return pokemons;
     }
 
 
